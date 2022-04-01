cbcc93feffcf754047a644bd6a86a8e211015ee2
Jesse Fluck <jesse.fluck@gmail.com>
2022-04-01 11:25:50 +0200

    mise en place du script js

--- front/js/script.js
+++ front/js/script.js
@@ -1,48 +1,29 @@
 // Récupération des données de l'API
 
-fetch("http://localhost:3000/api/products")
-    .then(function(res) {
-        if (res.ok){
-            return res.json();
-        }
-    })
-    .then(function(showAPI) {
-        const articles = showAPI;
-        console.table(articles);
+fetch("http://localhost:3000/api/products/")
+
+.then(function(res){
+    if(res.ok){
+        return res.json();
+    }
+})
 
 
+// Création de chaque élément des produits avec interpolation de variables
 
-        // Boucle 1 article par index
-        for (let article in articles) {
+.then((products) => {
+    for (let product of products){
+        document.querySelector("#items").innerHTML += ` <a href="./product.html?id=${product._id}">
+        <article>
+          <img src="${product.imageUrl}" alt="${product.altTxt}">
+          <h3 class="productName">${product.name}</h3>
+          <p class="productDescription">${product.description}</p>
+        </article>
+      </a> `
+    }
+})
 
-            // Liens dynamiques
-            let productUrl = document.createElement("a");
-            document.querySelector(".items").appendChild(productUrl);
-            productUrl.href = 'product.html?id=' + showAPI[article]._id;
 
-            // Articles dynamiques
-            let productArticle = document.createElement("article");
-            productUrl.appendChild(productArticle);
-
-            // Images dynamiques
-            let productImg = document.createElement("img");
-            productArticle.appendChild(productImg);
-            productImg.src= showAPI[article].imageUrl;
-            productImg.alt= showAPI[article].altTxt;
-
-            // Nom de l'article dynamique
-            let productName = document.createElement("h3");
-            productArticle.appendChild(productName);
-            productName.classList = "productName";
-            productName.innerHTML = showAPI[article].name;
-
-            // Description de l'article
-            let productDescription = document.createElement("p");
-            productArticle.appendChild(productDescription);
-            productDescription.classList = "productDescription";
-            productDescription.innerHTML = showAPI[article].description;
-        }
-    })
-    .catch(function(error) {
-        console.log(error);
-    })
+.catch(function(error) {
+    console.log(error);
+})
